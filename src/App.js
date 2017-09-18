8b2cff8dd826c92c886901af0b9675020246dfc4
Chris <chris@usablehq.com>
2017-09-18 06:48:41 +0100

    Updated style

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Ooer
+        <div className="Menu">
+          <span>Welcome to Ooer</span>
+          <a href="ooer.com">Blog</a>
+          <a href="github.com">Github</a>
+          <a href="twitter.com">Twitter</a>
+          <a href="linkedin.com">LinkedIn</a>
+          <a href="synth.ooer.com">Synthboard</a>
+          <a href="test.ooer.com">Test The Web</a>
+          <a href="decks.ooer.com">Decks</a>
+        </div>
       </div>
     );
   }
