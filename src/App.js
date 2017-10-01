d635b8685b1e8734354cdbcbea92fe906655ea03
Chris <chris@usablehq.com>
2017-10-01 15:33:57 +0100

    Title updated

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ class App extends Component {
     return (
       <nav>
         <h1>Welcome</h1>
-        <a href="http://about.ooer.com">About</a>
-        <a href="http://blog.ooer.com">Blog</a>
+        <a href="https://blog.ooer.com">Blog</a>
         <a href="https://github.com/onion2k">Github</a>
         <a href="https://twitter.com/onion2k">Twitter</a>
         <a href="https://linkedin.com/onion2k">LinkedIn</a>
         <a href="http://test.ooer.com">Test The Web</a>
-        <a href="http://yt1210.ooer.com">YT1210</a>
+        <a href="https://yt1210.ooer.com">YT1210</a>
         <a href="https://synth.ooer.com">Synth</a>
         <a href="http://coe.ooer.com">Crispy</a>
-        <a href="http://www.ooer.com/toys">Toybox</a>
       </nav>
     );
   }
