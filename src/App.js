c02f726774959e16e5aa4e6027df830d3733d728
Chris <chris@usablehq.com>
2017-10-12 16:27:35 +0100

    Updated homepage

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <nav>
-        <h1>Welcome</h1>
-        <a href="https://blog.ooer.com">Blog</a>
-        <a href="https://github.com/onion2k">Github</a>
-        <a href="https://twitter.com/onion2k">Twitter</a>
-        <a href="https://linkedin.com/onion2k">LinkedIn</a>
-        <a href="http://test.ooer.com">Test The Web</a>
-        <a href="https://yt1210.ooer.com">YT1210</a>
-        <a href="https://synth.ooer.com">Synth</a>
-        <a href="http://coe.ooer.com">Crispy</a>
-      </nav>
+      <div>
+        <h1>Ooer</h1>
+        <nav>
+          <a href="https://blog.ooer.com">Blog</a>
+          <a href="https://github.com/onion2k">Github</a>
+          <a href="https://twitter.com/onion2k">Twitter</a>
+          <a href="https://linkedin.com/onion2k">LinkedIn</a>
+          <a href="http://test.ooer.com">Test The Web</a>
+          <a href="https://yt1210.ooer.com">YT1210</a>
+          <a href="https://synth.ooer.com">Synth</a>
+          <a href="http://coe.ooer.com">Crispy</a>
+          <a href="http://time.ooer.com">Time</a>
+          <a href="http://advent.ooer.com">Advent</a>
+          <a href="http://pixelheart.ooer.com">Pixelheart</a>
+        </nav>
+      </div>
     );
   }
 }
