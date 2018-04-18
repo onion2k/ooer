f500c19af0cd69f6c07d528b9af645952decbecb
Chris Neale <chris@komododigital.co.uk>
2018-04-18 08:42:56 +0100

    3 column layout

--- src/App.js
+++ src/App.js
@@ -5,30 +5,38 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Ooer</h1>
-        <nav>
-          <a href="https://blog.ooer.com">Blog</a>
-          <a href="https://github.com/onion2k">Github</a>
-          <a href="https://twitter.com/onion2k">Twitter</a>
-          <a href="https://www.linkedin.com/in/cdneale">LinkedIn</a>
-          <a href="http://test.ooer.com">Test The Web</a>
-          <a href="https://yt1210.ooer.com">YT1210</a>
-          <a href="https://synth.ooer.com">Synth</a>
-          <a href="http://coe.ooer.com">Crispy</a>
-          <a href="http://time.ooer.com">Time</a>
-          <a href="https://advent.ooer.com">Advent</a>
-          <a href="https://pixelheart.ooer.com">Pixelheart</a>
-          <a href="https://art.ooer.com">Art</a>
-          <a href="https://disco.ooer.com">Disco</a>
-          <a href="https://light.ooer.com">Light</a>
-          <a href="https://mccree.ooer.com">McCree</a>
-          <a href="https://autograph.ooer.com">Autograph</a>
-          <a href="https://imgsvg.ooer.com">IMGSVG</a>
-        </nav>
-      </div>
+      <main>
+        <div></div><div></div><div></div><div></div><div></div>
+        <div></div>
+        <article href="https://blog.ooer.com"><header>Blog</header></article>
+        <article href="https://github.com/onion2k"><header>Github</header></article>
+        <article href="https://twitter.com/onion2k"><header>Twitter</header></article>
+        <div></div><div></div>
+        <article href="https://www.linkedin.com/in/cdneale"><header>LinkedIn</header></article>
+        <article href="http://test.ooer.com"><header>Test The Web</header></article>
+        <article href="https://yt1210.ooer.com"><header>YT1210</header></article>
+        <div></div><div></div>
+        <article href="https://synth.ooer.com"><header>Synth</header></article>
+        <article href="http://coe.ooer.com"><header>Crispy</header></article>
+        <article href="http://time.ooer.com"><header>Time</header></article>
+        <div></div><div></div>
+        <article href="https://advent.ooer.com"><header>Advent</header></article>
+        <article href="https://pixelheart.ooer.com"><header>Pixelheart</header></article>
+        <article href="https://art.ooer.com"><header>Art</header></article>
+        <div></div><div></div>
+        <article href="https://disco.ooer.com"><header>Disco</header></article>
+        <article href="https://light.ooer.com"><header>Ligh</header>t</article>
+        <article href="https://mccree.ooer.com"><header>McCree</header></article>
+        <div></div><div></div>
+        <article href="https://autograph.ooer.com"><header>Autograph</header></article>
+        <article href="https://imgsvg.ooer.com"><header>IMGSVG</header></article>
+        <div></div><div></div>
+        <div></div><div></div><div></div><div></div><div></div>
+      </main>
     );
   }
 }
 
 export default App;
+
+
