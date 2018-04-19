7e45616e916bf0482da8cef5ac01982a2dbd274b
Chris Neale <chris@komododigital.co.uk>
2018-04-19 08:57:27 +0100

    Added captions

--- src/App.js
+++ src/App.js
@@ -36,23 +36,88 @@ class App extends Component {
             <p>Why not join my professional network? Besides the fact LinkedIn is awful of course.</p>
           </div>
         </article>
-        <article href="https://yt1210.ooer.com" className="color5"><header>YT1210</header></article>
-        <article href="https://light.ooer.com" className="color6"><header>Light</header></article>
+        <article href="https://yt1210.ooer.com" className="color5">
+          <header>YT1210</header>
+          <div>
+            <p>Decks (well, a deck) in React. Build a playlist of 6 tracks from YouTube and control them by dragging the record arm.</p>
+          </div>
+        </article>
+        <article href="https://light.ooer.com" className="color6">
+          <header>Light</header>
+          <div>
+            <p>A GLSL shader coupled to code that maps where divs are on the page, to give a nice "back light" effect.</p>
+          </div>
+        </article>
         <div></div><div></div>
-        <article href="https://synth.ooer.com" className="color7"><header>Synth</header></article>
-        <article href="http://coe.ooer.com" className="color8"><header>Crispy</header></article>
-        <article href="http://time.ooer.com" className="color9"><header>Time</header></article>
+        <article href="https://synth.ooer.com" className="color7">
+          <header>Synth</header>
+          <div>
+            <p>A React sequencer that plays samples from a timeline. Vaporwave!</p>
+          </div>
+        </article>
+        <article href="http://coe.ooer.com" className="color8">
+          <header>Crispy</header>
+          <div>
+            <p>A React gallery that displays a part of an image, and then replaces all of the thumbnails with the whole image when you hover over.</p>
+          </div>
+        </article>
+        <article href="http://time.ooer.com" className="color9">
+          <header>Time</header>
+          <div>
+            <p>A timeline of the Internet built using the same base HTML and updated by replacing the CSS.</p>
+          </div>
+        </article>
         <div></div><div></div>
-        <article href="https://advent.ooer.com" className="color10"><header>Advent</header></article>
-        <article href="https://pixelheart.ooer.com" className="color11"><header>Pixelheart</header></article>
-        <article href="https://art.ooer.com" className="color12"><header>Art</header></article>
+        <article href="https://advent.ooer.com" className="color10">
+          <header>Advent</header>
+          <div>
+            <p>An advent calendar in React.</p>
+          </div>
+        </article>
+        <article href="https://pixelheart.ooer.com" className="color11">
+          <header>Pixelheart</header>
+          <div>
+            <p>3D pixel hearts in three.js</p>
+          </div>
+        </article>
+        <article href="https://art.ooer.com" className="color12">
+          <header>Art</header>
+          <div>
+            <p>Arty experiments</p>
+          </div>
+        </article>
         <div></div><div></div>
-        <article href="https://disco.ooer.com" className="color13"><header>Disco</header></article>
-        <article href="https://mccree.ooer.com" className="color14"><header>McCree</header></article>
-        <article href="http://test.ooer.com" className="color15"><header>Test The Web</header></article>
+        <article href="https://disco.ooer.com" className="color13">
+          <header>Disco</header>
+          <div>
+            <p>three.js disco ducks</p>
+          </div>
+        </article>
+        <article href="https://mccree.ooer.com" className="color14">
+          <header>McCree</header>
+          <div>
+            <p>Shoot a page to make holes that light shines through.</p>
+          </div>
+        </article>
+        <article href="http://test.ooer.com" className="color15">
+          <header>Test The Web</header>
+          <div>
+            <p>A website about testing that isn't even close to finished.</p>
+          </div>
+        </article>
         <div></div><div></div>
-        <article href="https://autograph.ooer.com" className="color16"><header>Autograph</header></article>
-        <article href="https://imgsvg.ooer.com" className="color17"><header>IMGSVG</header></article>
+        <article href="https://autograph.ooer.com" className="color16">
+          <header>Autograph</header>
+          <div>
+            <p>Generate lovely art using a system similar to spirograph</p>
+          </div>
+        </article>
+        <article href="https://imgsvg.ooer.com" className="color17">
+          <header>IMGSVG</header>
+          <div>
+            <p>Turn images in to SVG graphics using tessellating shapes.</p>
+          </div>
+        </article>
         <div></div><div></div>
         <div></div><div></div><div></div><div></div><div></div>
       </main>
