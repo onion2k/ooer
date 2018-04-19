a52039dcfef896b30f653efb8db0820dd97050d9
Chris Neale <chris@komododigital.co.uk>
2018-04-19 08:41:14 +0100

    Title and gradients

--- src/App.js
+++ src/App.js
@@ -8,16 +8,19 @@ class App extends Component {
       <main>
         <div></div><div></div><div></div><div></div><div></div>
         <div></div>
-        <article href="https://blog.ooer.com" className="color1 blog">
-          <header>Blog</header>
-          <div>
-            <p>I don't really write a blog but occasionally I dump something in to a file somewhere and it makes it's way on to the web.</p>
-          </div>
-        </article>
-        <article href="https://github.com/onion2k" className="color2">
+          <div className="title"><h1>Ooer</h1></div>
+        <div></div>
+        <div></div>
+        <article href="https://github.com/onion2k" className="color1">
           <header>Github</header>
           <div>
             <p>I write code every day, and most of it is open source and freely available on Github. Clone all the things!</p>
+          </div>
+        </article>
+        <article href="https://blog.ooer.com" className="color2 blog">
+          <header>Blog</header>
+          <div>
+            <p>I don't really write a blog but occasionally I dump something in to a file somewhere and it makes it's way on to the web.</p>
           </div>
         </article>
         <article href="https://twitter.com/onion2k" className="color3">
@@ -33,23 +36,23 @@ class App extends Component {
             <p>Why not join my professional network? Besides the fact LinkedIn is awful of course.</p>
           </div>
         </article>
-        <article href="https://yt1210.ooer.com" className="color1"><header>YT1210</header></article>
-        <article href="https://light.ooer.com" className="color4"><header>Light</header></article>
+        <article href="https://yt1210.ooer.com" className="color5"><header>YT1210</header></article>
+        <article href="https://light.ooer.com" className="color6"><header>Light</header></article>
         <div></div><div></div>
-        <article href="https://synth.ooer.com" className="color2"><header>Synth</header></article>
-        <article href="http://coe.ooer.com" className="color3"><header>Crispy</header></article>
-        <article href="http://time.ooer.com" className="color4"><header>Time</header></article>
+        <article href="https://synth.ooer.com" className="color7"><header>Synth</header></article>
+        <article href="http://coe.ooer.com" className="color8"><header>Crispy</header></article>
+        <article href="http://time.ooer.com" className="color9"><header>Time</header></article>
         <div></div><div></div>
-        <article href="https://advent.ooer.com" className="color5"><header>Advent</header></article>
-        <article href="https://pixelheart.ooer.com" className="color1"><header>Pixelheart</header></article>
-        <article href="https://art.ooer.com" className="color2"><header>Art</header></article>
+        <article href="https://advent.ooer.com" className="color10"><header>Advent</header></article>
+        <article href="https://pixelheart.ooer.com" className="color11"><header>Pixelheart</header></article>
+        <article href="https://art.ooer.com" className="color12"><header>Art</header></article>
         <div></div><div></div>
-        <article href="https://disco.ooer.com" className="color3"><header>Disco</header></article>
-        <article href="https://mccree.ooer.com" className="color5"><header>McCree</header></article>
-        <article href="http://test.ooer.com" className="color5"><header>Test The Web</header></article>
+        <article href="https://disco.ooer.com" className="color13"><header>Disco</header></article>
+        <article href="https://mccree.ooer.com" className="color14"><header>McCree</header></article>
+        <article href="http://test.ooer.com" className="color15"><header>Test The Web</header></article>
         <div></div><div></div>
-        <article href="https://autograph.ooer.com" className="color1"><header>Autograph</header></article>
-        <article href="https://imgsvg.ooer.com" className="color2"><header>IMGSVG</header></article>
+        <article href="https://autograph.ooer.com" className="color16"><header>Autograph</header></article>
+        <article href="https://imgsvg.ooer.com" className="color17"><header>IMGSVG</header></article>
         <div></div><div></div>
         <div></div><div></div><div></div><div></div><div></div>
       </main>
