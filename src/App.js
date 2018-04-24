b8c5c8d3b83e2916c2d3bbb8292607483c1bdd70
Chris Neale <chris@komododigital.co.uk>
2018-04-24 09:09:23 +0100

    Reworking layout

--- src/App.js
+++ src/App.js
@@ -1,135 +1,41 @@
 import React, { Component } from 'react';
+import LinkArticle from './LinkArticle';
 
 import './App.css';
 
+const data = [
+  { href:"https://github.com/onion2k", title:"Github", description:"I write code every day, and most of it is open source and freely available on Github. Clone all the things!", color:"color1" },
+  { href:"https://blog.ooer.com", title:"Blog", description:"I don't really write a blog but occasionally I dump something in to a file somewhere and it makes it's way on to the web.", color:"color2" },
+  { href:"https://www.linkedin.com/in/cdneale", title:"LinkedIn", description:"Why not join my professional network? Besides the fact LinkedIn is awful of course.", color:"color4" },
+  { href:"https://yt1210.ooer.com", title:"YT1210", description:"Decks (well, a deck) in React. Build a playlist of 6 tracks from YouTube and control them by dragging the record arm.", color:"color5" },
+  { href:"https://light.ooer.com", title:"Light", description:"A GLSL shader coupled to code that maps where divs are on the page, to give a nice back light effect.", color:"color6" },
+  { href:"https://synth.ooer.com", color:"color7", title:"Synth", description:"A React sequencer that plays samples from a timeline. Vaporwave!" },
+  { href:"http://coe.ooer.com", color:"color8", title:"Crispy", description:"A React gallery that displays a part of an image, and then replaces all of the thumbnails with the whole image when you hover over." },
+  { href:"http://time.ooer.com", color:"color9", title:"Time", description:"A timeline of the Internet built using the same base HTML and updated by replacing the CSS." },
+  { href:"https://advent.ooer.com", color:"color10", title:"Advent", description:"An advent calendar in React." },
+  { href:"https://pixelheart.ooer.com", color:"color11", title:"Pixelheart", description:"3D pixel hearts in three.js" },
+  { href:"https://art.ooer.com", color:"color12", title:"Art", description:"Arty experiments" },
+  { href:"https://disco.ooer.com", color:"color13", title:"Disco", description:"three.js disco ducks" },
+  { href:"https://mccree.ooer.com", color:"color14", title:"McCree", description:"Shoot a page to make holes that light shines through." },
+  { href:"http://test.ooer.com", color:"color15", title:"Test", description:"A website about testing that isn't even close to finished." },
+  { href:"https://autograph.ooer.com", color:"color16", title:"Autograph", description:"Generate lovely art using a system similar to spirograph" },
+  { href:"https://imgsvg.ooer.com", color:"color17", title:"IMGSVG", description:"Turn images in to SVG graphics using tessellating shapes." },
+  { href:"https://mandala.ooer.com", color:"color18", title:"Mandala", description:"A canvas manadala generator." }
+]
+
 class App extends Component {
   render() {
+
+    let links = data.map((d)=>{
+      return <LinkArticle href={d.href} color={d.color} title={d.title} description={d.description} />
+    });
+
     return (
       <main>
         <div></div><div></div><div></div><div></div><div></div>
-        <div className="entrance"></div>
-          <div className="title"><h1>Ooer</h1></div>
-        <div className="spacer"></div>
-        <div className="spacer"></div>
-        <a href="https://github.com/onion2k" className="color1">
-          <header>Github</header>
-          <div>
-            <p>I write code every day, and most of it is open source and freely available on Github. Clone all the things!</p>
-          </div>
-        </a>
-        <a href="https://blog.ooer.com" className="color2 blog">
-          <header>Blog</header>
-          <div>
-            <p>I don't really write a blog but occasionally I dump something in to a file somewhere and it makes it's way on to the web.</p>
-          </div>
-        </a>
-        <a href="https://twitter.com/onion2k" className="color3">
-          <header>Twitter</header>
-          <div>
-            <p>I've been on Twitter for ages, but I don't tweet as much as I used to. I retweet a lot of tech, code, diversity and silly jokes.</p>
-          </div>
-        </a>
-        <div className="spacer"></div><div className="spacer"></div>
-        <a href="https://www.linkedin.com/in/cdneale" className="color4">
-          <header>LinkedIn</header>
-          <div>
-            <p>Why not join my professional network? Besides the fact LinkedIn is awful of course.</p>
-          </div>
-        </a>
-        <a href="https://yt1210.ooer.com" className="color5">
-          <header>YT1210</header>
-          <div>
-            <p>Decks (well, a deck) in React. Build a playlist of 6 tracks from YouTube and control them by dragging the record arm.</p>
-          </div>
-        </a>
-        <a href="https://light.ooer.com" className="color6">
-          <header>Light</header>
-          <div>
-            <p>A GLSL shader coupled to code that maps where divs are on the page, to give a nice "back light" effect.</p>
-          </div>
-        </a>
-        <div className="spacer"></div><div className="spacer"></div>
-        <a href="https://synth.ooer.com" className="color7">
-          <header>Synth</header>
-          <div>
-            <p>A React sequencer that plays samples from a timeline. Vaporwave!</p>
-          </div>
-        </a>
-        <a href="http://coe.ooer.com" className="color8">
-          <header>Crispy</header>
-          <div>
-            <p>A React gallery that displays a part of an image, and then replaces all of the thumbnails with the whole image when you hover over.</p>
-          </div>
-        </a>
-        <a href="http://time.ooer.com" className="color9">
-          <header>Time</header>
-          <div>
-            <p>A timeline of the Internet built using the same base HTML and updated by replacing the CSS.</p>
-          </div>
-        </a>
-        <div className="spacer"></div><div className="spacer"></div>
-        <a href="https://advent.ooer.com" className="color10">
-          <header>Advent</header>
-          <div>
-            <p>An advent calendar in React.</p>
-          </div>
-        </a>
-        <a href="https://pixelheart.ooer.com" className="color11">
-          <header>Pixelheart</header>
-          <div>
-            <p>3D pixel hearts in three.js</p>
-          </div>
-        </a>
-        <a href="https://art.ooer.com" className="color12">
-          <header>Art</header>
-          <div>
-            <p>Arty experiments</p>
-          </div>
-        </a>
-        <div className="spacer"></div><div className="spacer"></div>
-        <a href="https://disco.ooer.com" className="color13">
-          <header>Disco</header>
-          <div>
-            <p>three.js disco ducks</p>
-          </div>
-        </a>
-        <a href="https://mccree.ooer.com" className="color14">
-          <header>McCree</header>
-          <div>
-            <p>Shoot a page to make holes that light shines through.</p>
-          </div>
-        </a>
-        <a href="http://test.ooer.com" className="color15">
-          <header>Test</header>
-          <div>
-            <p>A website about testing that isn't even close to finished.</p>
-          </div>
-        </a>
-        <div className="spacer"></div><div className="spacer"></div>
-        <a href="https://autograph.ooer.com" className="color16">
-          <header>Autograph</header>
-          <div>
-            <p>Generate lovely art using a system similar to spirograph</p>
-          </div>
-        </a>
-        <a href="https://imgsvg.ooer.com" className="color17">
-          <header>IMGSVG</header>
-          <div>
-            <p>Turn images in to SVG graphics using tessellating shapes.</p>
-          </div>
-        </a>
-        <a href="https://mandala.ooer.com" className="color18">
-          <header>Mandala</header>
-          <div>
-            <p>A canvas manadala generator.</p>
-          </div>
-        </a>
-        <div className="spacer"></div>
-
-        <div></div>
-          <div className="footer"><h2>Thanks for visiting</h2></div>
-        <div></div>
-
+        <div className="title"><h1>Ooer</h1></div>
+        {links}
+        <div className="footer"><h2>Thanks for visiting</h2></div>
         <div></div><div></div><div></div><div></div><div></div>
       </main>
     );
